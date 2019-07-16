2ff03e9a30a9ce28982e53cea6ba3d055728132f
Lucas Cabral <lucas.kabral2112@gmail.com>
2019-07-16 18:24:26 -0300

    Adição de validação

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ function jogar(){
     }
 }
 
+
 function mudaValor(event) {
     event.target.innerHTML = valor;
+    jogar();
+    val();
 }
-
 
 function clicar() {
     for (let campo of campos) {
@@ -28,10 +30,43 @@ function clicar() {
     jogar();
 }
 
-// function validar() {
-//     if (campos[0].innerHTML == "x" && campos[1].innerHTML == "x" && campos[2].innerHTML == "x") {
-//         alert("vc")
-//     }
+function validar() {
+    if (campos[0].innerHTML == "X" && campos[1].innerHTML == "X" && campos[2].innerHTML == "X") {
+        alert("vc")
+    } else if(campos[3].innerHTML == "X" && campos[4].innerHTML == "X" && campos[5].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[6].innerHTML == "X" && campos[7].innerHTML == "X" && campos[8].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[0].innerHTML == "X" && campos[3].innerHTML == "X" && campos[6].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[1].innerHTML == "X" && campos[4].innerHTML == "X" && campos[7].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[2].innerHTML == "X" && campos[5].innerHTML == "X" && campos[8].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[0].innerHTML == "X" && campos[4].innerHTML == "X" && campos[8].innerHTML == "X"){
+        alert("vc")
+    }else if(campos[2].innerHTML == "X" && campos[4].innerHTML == "X" && campos[6].innerHTML == "X"){
+        alert("vc")
+    }
+    //  validação bolinha
+    else if(campos[0].innerHTML == "O" && campos[1].innerHTML == "O" && campos[2].innerHTML == "O") {
+        alert("vc")
+    } else if(campos[3].innerHTML == "O" && campos[4].innerHTML == "O" && campos[5].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[6].innerHTML == "O" && campos[7].innerHTML == "O" && campos[8].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[0].innerHTML == "O" && campos[3].innerHTML == "O" && campos[6].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[1].innerHTML == "O" && campos[4].innerHTML == "O" && campos[7].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[2].innerHTML == "O" && campos[5].innerHTML == "O" && campos[8].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[0].innerHTML == "O" && campos[4].innerHTML == "O" && campos[8].innerHTML == "O"){
+        alert("vc")
+    }else if(campos[2].innerHTML == "O" && campos[4].innerHTML == "O" && campos[6].innerHTML == "O"){
+        alert("vc")
+    }
+}
 
-// }
 
+ clicar();
