a697d63030c17df63ac67626df74d5e3fb2a9c8a
Lucas Cabral <lucas.kabral2112@gmail.com>
2019-07-18 10:34:52 -0300

    Algumas alterações - ignorar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let campos = document.querySelectorAll(".campo");
 let jog1 = true;
 let jog2 = false;
-let jogadas = 1;//var p ver o num de jogadas
+let jogadas = 0;//var p ver o num de jogadas
 
 function jogar() {
     if (jog1 == true && jog2 == false) {
@@ -19,9 +19,16 @@ function jogar() {
 
 
 function mudaValor(event) {
-    event.target.innerHTML = valor;
-    jogar();
-    validar();
+    if(jogadas < 10){
+        event.target.innerHTML = valor;
+        event.target.onclick = null;
+        jogar();
+        validar();
+    }
+    if(jogadas >= 10 && validar.call() == false){
+        alert('Deu velha');
+        limpar();
+    }
 }
 
 function clicar() {
@@ -31,41 +38,85 @@ function clicar() {
     jogar();
 }
 
-function validar() {
+function validar(bloquado) {
+    bloquado = false;
     if (campos[0].innerHTML == "X" && campos[1].innerHTML == "X" && campos[2].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[3].innerHTML == "X" && campos[4].innerHTML == "X" && campos[5].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[6].innerHTML == "X" && campos[7].innerHTML == "X" && campos[8].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[0].innerHTML == "X" && campos[3].innerHTML == "X" && campos[6].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[1].innerHTML == "X" && campos[4].innerHTML == "X" && campos[7].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[2].innerHTML == "X" && campos[5].innerHTML == "X" && campos[8].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[0].innerHTML == "X" && campos[4].innerHTML == "X" && campos[8].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     } else if (campos[2].innerHTML == "X" && campos[4].innerHTML == "X" && campos[6].innerHTML == "X") {
         alert("vc")
+        bloquado = true;
     }
     //  validação bolinha
-    else if (campos[0].innerHTML == "O" && campos[1].innerHTML == "O" && campos[2].innerHTML == "O") {
+    if (campos[0].innerHTML == "O" && campos[1].innerHTML == "O" && campos[2].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[3].innerHTML == "O" && campos[4].innerHTML == "O" && campos[5].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[6].innerHTML == "O" && campos[7].innerHTML == "O" && campos[8].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[0].innerHTML == "O" && campos[3].innerHTML == "O" && campos[6].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[1].innerHTML == "O" && campos[4].innerHTML == "O" && campos[7].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[2].innerHTML == "O" && campos[5].innerHTML == "O" && campos[8].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[0].innerHTML == "O" && campos[4].innerHTML == "O" && campos[8].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
     } else if (campos[2].innerHTML == "O" && campos[4].innerHTML == "O" && campos[6].innerHTML == "O") {
         alert("vc")
+        bloquado = true;
+    }
+
+    if(bloquado == true){
+        impedeClick();
+    }
+
+}
+
+function impedeClick(){
+    for(let campo of campos){
+        campo.onclick = null;
+    }
+}
+
+function limpar(){
+    for(let campo of campos){
+        campo.onclick = initial;
+    }
+}
+
+
+
+function val() {
+    if (jogadas = 10) {
+        validar();
+    } else {
+        alert('Deu velha');
     }
 }
 
